f26101746c9fe09c55a2791df2f81678d211efc4
Jen <wonee0321@gmail.com>
2021-09-09 20:38:40 +0900

    [FEAT]SceneCoordinator 구현1

--- RxMemo/Scene/SceneCoordinator.swift
+++ RxMemo/Scene/SceneCoordinator.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+
+//화면 전환 담당
+class SceneCoordinator: SceneCoordinatorType {
+    
+    private let bag = DisposeBag()
+    private var window: UIWindow
+    private var currentVC: UIViewController
+    
+    required init(window: UIWindow) {
+        self.window = window
+        currentVC = window.rootViewController!
+    }
+    
+    @discardableResult
+    func transtion(to scene: Scene, using style: TransitionStyle, animated: Bool) -> Completable {
+        <#code#>
+    }
+    
+    
+    @discardableResult
+    func close(animated: Bool) -> Completable {
+        <#code#>
+    }
+    
+    
+}
