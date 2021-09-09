203e19489c5d499d3c8461e759e69135246dd038
Jen <wonee0321@gmail.com>
2021-09-09 20:32:34 +0900

    [UPDATE] SceneCoordinatorType 구현

--- RxMemo/Scene/SceneCoordinatorType.swift
+++ RxMemo/Scene/SceneCoordinatorType.swift
@@ -6,4 +6,12 @@
 //
 
 import Foundation
+import RxSwift
 
+protocol SceneCoordinatorType {
+    @discardableResult
+    func transtion(to scene: Scene, using style: TransitionStyle, animated: Bool) -> Completable
+    
+    @discardableResult
+    func close(animated: Bool) -> Completable 
+}
